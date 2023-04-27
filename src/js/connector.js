09a999fd24ada43bb45e6a70ce0d600120a81aba
abteenz <abteenz@gmail.com>
2023-04-26 23:45:50 -0700

    added gpt code

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,52 @@
-console.log('Hello world!');
+const t = window.TrelloPowerUp.iframe();
 
-window.TrelloPowerUp.initialize({
-    'card-badges': function(t, opts) {
-        //retun an arrray of card badges for the given card
-    }
+// Step 2: Authenticate with Trello
+t.initialize({
+  'authorization': {
+    'interactive': true,
+    'expiration': 'never',
+    'scope': {
+      'read': 'allow',
+      'write': 'allow'
+    },
+    'name': 'My Trello Power-Up'
+  },
+  'appKey': 'YOUR_APP_KEY'
 });
+
+// Step 3: Subscribe to comment events
+t.board('id').then(function(board) {
+  trello.get(`/1/boards/${board.id}/actions?filter=commentCard&limit=1000`)
+    .then(function(actions) {
+      actions.forEach(function(action) {
+        if (action.data.text.match(/^(\d{2}:){2}\d{2}$/)) {
+          // Step 4: Parse comments for 00:00:00 format
+          let commentValue = action.data.text;
+          let sumValue = 0;
+          t.card('id', 'name').then(function(card) {
+            let currentTitle = card.name;
+            if (currentTitle.indexOf('|') !== -1) {
+              sumValue = parseInt(currentTitle.split('|')[1]);
+              currentTitle = currentTitle.split('|')[0].trim();
+            }
+            sumValue += parseInt(commentValue.split(':')[0]) * 3600 + parseInt(commentValue.split(':')[1]) * 60 + parseInt(commentValue.split(':')[2]);
+            t.card('name', `${currentTitle} | ${sumValue}`).then(function() {
+              // Step 5: Update the card title
+              console.log('Card title updated.');
+            });
+          });
+        }
+      });
+    });
+});
+
+// Step 6: Handle errors
+t.render(function() {
+  if (t.memberCanWriteToModel('card')) {
+    console.log('User has write permission.');
+  } else {
+    console.log('User does not have write permission.');
+  }
+});
+
+// Step 7: Test and deploy
