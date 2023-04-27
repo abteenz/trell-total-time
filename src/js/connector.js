726891ed561703300cc8964e3885e118a1abbd91
abteenz <abteenz@gmail.com>
2023-04-26 23:55:28 -0700

    basic card badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,5 +3,12 @@ console.log('Hello world!');
 window.TrelloPowerUp.initialize({
     'card-badges': function(t, opts) {
         //retun an arrray of card badges for the given card
+        return t.card('all')
+        .then(function(card){
+            console.log(card);
+            return [{
+                text: card.idShort
+            }]
+        })
     }
 });
