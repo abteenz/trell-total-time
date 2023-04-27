51b8158a477ab3d646a18119fdbbdeff49361475
abteenz <abteenz@gmail.com>
2023-04-27 00:18:35 -0700

    new code

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,52 +1,24 @@
 const t = window.TrelloPowerUp.iframe();
 
-// Step 2: Authenticate with Trello
-t.initialize({
-  'authorization': {
-    'interactive': true,
-    'expiration': 'never',
-    'scope': {
-      'read': 'allow',
-      'write': 'allow'
-    },
-    'name': 'My Trello Power-Up'
-  },
-  'appKey': 'Y9dfb2370fa657c0d8e1d0e4802d9823d'
-});
-console.log('ABTIN IS HERE!!');
-// Step 3: Subscribe to comment events
-t.board('id').then(function(board) {
-  trello.get(`/1/boards/${board.id}/actions?filter=commentCard&limit=1000`)
-    .then(function(actions) {
-      actions.forEach(function(action) {
-        if (action.data.text.match(/^(\d{2}:){2}\d{2}$/)) {
-          // Step 4: Parse comments for 00:00:00 format
-          let commentValue = action.data.text;
-          let sumValue = 0;
-          t.card('id', 'name').then(function(card) {
-            let currentTitle = card.name;
-            if (currentTitle.indexOf('|') !== -1) {
-              sumValue = parseInt(currentTitle.split('|')[1]);
-              currentTitle = currentTitle.split('|')[0].trim();
-            }
-            sumValue += parseInt(commentValue.split(':')[0]) * 3600 + parseInt(commentValue.split(':')[1]) * 60 + parseInt(commentValue.split(':')[2]);
-            t.card('name', `${currentTitle} | ${sumValue}`).then(function() {
-              // Step 5: Update the card title
-              console.log('Card title updated.');
-            });
-          });
-        }
-      });
-    });
-});
+t.render(async function () {
+  // Get all the card comments
+  const comments = await t.card('all').get('comments');
+  
+  // Filter comments with the format "00:00:00"
+  const filteredComments = comments.filter(comment => /^\d{2}:\d{2}:\d{2}$/.test(comment.data.text));
 
-// Step 6: Handle errors
-t.render(function() {
-  if (t.memberCanWriteToModel('card')) {
-    console.log('User has write permission.');
-  } else {
-    console.log('User does not have write permission.');
-  }
-});
+  // Sum up the values of the filtered comments
+  const sum = filteredComments.reduce((total, comment) => {
+    const time = comment.data.text.split(':').map(Number);
+    return total + (time[0] * 3600 + time[1] * 60 + time[2]);
+  }, 0);
 
-// Step 7: Test and deploy
+  // Get the current card title
+  const cardTitle = await t.card('name').get('name');
+
+  // Append the sum to the card title
+  const newTitle = `${cardTitle} | ${new Date(sum * 1000).toISOString().substr(11, 8)}`;
+
+  // Update the card title
+  return t.card('name').set({ name: newTitle });
+});
