b200fbb282cffbd37341acb1c0283d45fd9e8b3e
abteenz <abteenz@gmail.com>
2023-05-21 13:14:43 -0700

    maybe this time?

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,37 +1,35 @@
-window.TrelloPowerUp.initialize({
-  'card-badges': function(t, opts) {
-    let cardName;
-    let today = new Date();
-    let yyyy = today.getFullYear();
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); 
-    let dd = String(today.getDate()).padStart(2, '0'); 
-    let currentDate = `${yyyy}-${mm}-${dd}`;
+var Promise = TrelloPowerUp.Promise;
+var t = TrelloPowerUp.iframe();
 
-    return t.card('name')
-      .get('name')
-      .then(function(name) {
-        cardName = name;
-        // Instead of directly calling Toggl API, we call our server-side script
-        // which will make the request to Toggl API and return the response.
-        return fetch("https://incredible-naiad-5425d4.netlify.app/.netlify/functions/get-toggl-total?description=" + encodeURIComponent(cardName) + "&start_date=2023-01-01&end_date=" + currentDate, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*", // Add this line
-           },
-      })
-      .then(response => response.json())
-      .then(function(json) {
-        const totalTime = json.total_grand || 0;
-        return [{
-          dynamic: function() {
-            return {
-              text: totalTime + ' total time',
-              icon: './images/icon.svg',
-              color: 'green'
-            }
-          }
-        }];
-      });
-  }
+// we can access arguments passed to the iframe like so
+var arg = t.arg('arg');
+
+t.render(function(){
+  return Promise.all([
+    t.get('card', 'name'),
+    t.get('board', 'prefs')
+  ])
+  .spread(function(cardName, prefs){
+    // now that we have the card name and the board prefs we can do things with them
+    var currentDate = new Date().toISOString().split('T')[0]; // get current date in "yyyy-mm-dd" format
+
+    return fetch("https://incredible-naiad-5425d4.netlify.app/.netlify/functions/get-toggl-total?description=" + encodeURIComponent(cardName) + "&start_date=2023-01-01&end_date=" + currentDate, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Add this line
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      // do something with the data from the fetch request
+      console.log(data);
+    })
+    .catch(error => console.log('error', error));
+  })
+  .then(function(){
+    // then we will return a single promise that will resolve prior to us trying to call 'sizeTo'
+    return t.sizeTo('#content')
+    .done();
+  })
 });
