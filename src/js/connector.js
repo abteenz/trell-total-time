a6d9f372537e7d4dcd5bfa43ea8035e224b9ad6f
abteenz <abteenz@gmail.com>
2023-05-21 11:27:45 -0700

    dynamic proper

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,27 +1,33 @@
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
+    let cardName;
     return t.card('name')
       .get('name')
-      .then(function(cardName) {
-        // Replace 'your_toggl_api_token' with your actual Toggl API token.
-        var headers = new Headers();
-        headers.append("Authorization", "Basic " + btoa('a93a1bdec1193f3e9da5036c4f0a2e5a' + ":api_token"));
-
-        return t.request({
-          url: `https://api.track.toggl.com/reports/api/v2/details?user_agent=api_test&description=${cardName}`,
-          headers: headers
-        })
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          var totalTime = data.total_grand / 1000 / 60 / 60;  // total time in hours
-          return [{
-            text: totalTime.toFixed(2) + ' hrs',
-            icon: 'https://your/icon/urhttps://cdn.onlinewebfonts.com/svg/img_563942.png', // replace with your icon URL
-            color: null,
-          }];
+      .then(function(name) {
+        cardName = name;
+        return fetch("https://api.track.toggl.com/reports/api/v3/workspace/6759737/search/time_entries/totals", {  
+          method: "POST",  
+          headers: {  
+            "Content-Type": "application/json",  
+            "Authorization": `Basic ${btoa('a93a1bdec1193f3e9da5036c4f0a2e5a:api_token')}`
+          },
+          body: JSON.stringify({
+            "description": cardName
+          })  
         });
+      })
+      .then(response => response.json())
+      .then(function(json) {
+        const totalTime = json.total_grand || 0;
+        return [{
+          dynamic: function() {
+            return {
+              text: totalTime + ' total time',
+              icon: './images/icon.svg',
+              color: 'green'
+            }
+          }
+        }];
       });
-  },
+  }
 });
